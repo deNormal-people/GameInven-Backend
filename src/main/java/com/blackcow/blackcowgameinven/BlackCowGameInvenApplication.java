785a59793821e7db01a637b2295dceccd257ab0c
k971119 <hanyul97@naver.com>
2025-02-15 01:35:46 +0900

    초기 프로젝트 커밋

---
+++ src/main/java/com/blackcow/blackcowgameinven/BlackCowGameInvenApplication.java
@@ -0,0 +1,13 @@
+package com.blackcow.blackcowgameinven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackCowGameInvenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackCowGameInvenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackcow/blackcowgameinven/BlackCowGameInvenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackcow.blackcowgameinven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackCowGameInvenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
