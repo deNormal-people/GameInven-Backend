c884a9f8b4428c55c396e9d86f5f86a295717d9b
k971119 <hanyul97@naver.com>
2025-02-25 23:56:14 +0900

    사용자 권한 추가

--- src/main/java/com/blackcow/blackcowgameinven/service/CustomUserDetailsService.java
+++ src/main/java/com/blackcow/blackcowgameinven/service/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package com.blackcow.blackcowgameinven.service;
 import com.blackcow.blackcowgameinven.model.User;
 import com.blackcow.blackcowgameinven.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -21,7 +22,13 @@ public class CustomUserDetailsService implements UserDetailsService {
         User user = userRepository.findByUsername(username);
         if(user == null) new UsernameNotFoundException("User not found with username: " + username);
 
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
-                new ArrayList<>());
+        String roles = "USER";
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities(roles)
+                .authorities(new SimpleGrantedAuthority(roles))
+                .build();
     }
 }
