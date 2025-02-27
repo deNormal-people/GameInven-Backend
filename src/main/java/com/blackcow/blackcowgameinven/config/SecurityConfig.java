c84a5443f0b2eb5fc0afd58da3a6975f0941e625
k971119 <hanyul97@naver.com>
2025-02-27 22:05:02 +0900

    Bean 순환참조 오류 방지

--- src/main/java/com/blackcow/blackcowgameinven/config/SecurityConfig.java
+++ src/main/java/com/blackcow/blackcowgameinven/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.blackcow.blackcowgameinven.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -23,8 +24,17 @@ public class SecurityConfig{
     JwtAuthenticationFilter jwtAuthenticationFilter;
     OAuth2LoginSuccessHandler loginSuccessHandler;
 
+    /*
+    ┌─────┐
+    |  OAuth2LoginSuccessHandler  <-- (필드 주입) UserService
+    ↑     ↓
+    |  userService (필드 주입) PasswordEncoder
+    ↑     ↓
+    |  SecurityConfig  <-- (필드 주입) OAuth2LoginSuccessHandler
+    └─────┘
+     */
     @Autowired
-    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, OAuth2LoginSuccessHandler loginSuccessHandler) {
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, /*빈 순환참조 방지*/@Lazy OAuth2LoginSuccessHandler loginSuccessHandler) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
         this.loginSuccessHandler = loginSuccessHandler;
     }
