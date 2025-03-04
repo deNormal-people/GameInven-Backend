c7f97cdb0a79cb8064097a2fe64a8f9a6e1bcf97
k971119 <hanyul97@naver.com>
2025-03-04 21:24:55 +0900

    테스트코드 작성

--- src/test/java/com/blackcow/blackcowgameinven/UserTest/UserRepositoryTest.java
+++ src/test/java/com/blackcow/blackcowgameinven/UserTest/UserRepositoryTest.java
@@ -1,7 +1,9 @@
-package com.blackcow.blackcowgameinven.RepositoryTest;
+package com.blackcow.blackcowgameinven.UserTest;
 
 import com.blackcow.blackcowgameinven.model.User;
 import com.blackcow.blackcowgameinven.repository.UserRepository;
+import com.blackcow.blackcowgameinven.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,19 +11,35 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
-@ActiveProfiles("connection")
+@ActiveProfiles("test")
 @Rollback(value = true)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @MockitoBean
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        User user = User.builder()
+                .username("asdf")
+                .password("asdf")
+                .role("ADMIN")
+                .phone("123456789")
+                .email("asdf@gmail.com")
+                .build();
+        userRepository.save(user);
+    }
 
     @Test
     @DisplayName("사용자 조회")
@@ -30,6 +48,13 @@ public class UserRepositoryTest {
         List<User> userList = userRepository.findAllByUsername("asdf");
 
         assertEquals(1, userList.size());
+    }
+
+    @Test
+    @DisplayName("중복체크")
+    void 중복체크(){
+        //중복됨
+        assertEquals(false, userService.duplicationCheck("asdf"));
     }
 
 }
