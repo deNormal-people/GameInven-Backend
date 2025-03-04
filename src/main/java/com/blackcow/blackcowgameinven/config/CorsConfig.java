2742c7ed49acb6644f07f8a70f9c9e13ecf18114
k971119 <hanyul97@naver.com>
2025-03-04 21:29:19 +0900

    cors 추가

---
+++ src/main/java/com/blackcow/blackcowgameinven/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.blackcow.blackcowgameinven.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("http://localhost:3030"));  // 모든 Origin 허용 (프론트엔드 주소만 허용 가능)
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+}
