fc603762c034e81a70e2ba0a3c0bcef4987aabd6
k971119 <hanyul97@naver.com>
2025-03-11 10:29:23 +0900

    빌드오류 수정 (#9)
    * Update UserEndToEndTest.java
    
    * API DOCS 빌드시 포함 빌드 수정

--- src/main/java/com/blackcow/blackcowgameinven/controller/Docs.java
+++ src/main/java/com/blackcow/blackcowgameinven/controller/Docs.java
@@ -2,14 +2,17 @@ package com.blackcow.blackcowgameinven.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class Main {
 
-    @GetMapping("/")
+@Controller
+@RequestMapping("/docs")
+public class Docs {
+
+    @GetMapping("/blackcow-api")
     public String main(){
-        return "hi";
+        return "docs/restful API Manual";
     }
 
 }
